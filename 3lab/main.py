6f82543102513f81120052553d558cad4658ff20
Газизова Надежда ПИ25-3 <Nadushka1399@gmail.com>
2025-10-21 13:42:16 +0300

    Update main.py

--- 3lab/main.py
+++ 3lab/main.py
@@ -1,4 +1,13 @@
+def gen_per(elements, prefix=""):
+    if len(elements) == 0:
+        print(prefix)  #вывод каждой найденной перестановки
+    else:
+        for i in range(len(elements)):
+            cur_elem = elements[i]
+            ost_elem = elements[:i] + elements[i + 1:] #оставшиеся элементы после текущего
 
+            gen_per(ost_elem, prefix + str(cur_elem)) #pекурсивно генерируем оставшиеся перестановки
 
-if __name__ == "__main__":
-    pass # Ваш код здесь
+elements = '1234'
+print("Все перестановки:", elements)
+gen_per(list(elements))
